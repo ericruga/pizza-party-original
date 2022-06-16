896e5c48ee3e9c5d9bf4c19414778d7aec4e7edf
ericruga <ericruga@users.noreply.github.com>
2022-06-16 13:59:15 +0200

    Cambios antes de comer Jueves

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ const ctx = canvas.getContext("2d");
 const divMenu = document.getElementById("startScreen");
 const canvasDiv = document.getElementById("divCanvas");
 const startButton = document.getElementById("botonMenuInit");
+const restartButton = document.getElementById("botonRest");
+const ending = document.querySelector("#gameOver");
 
 // Declaración de las imagenes en Canvas
 let sliceImage = new Image();
@@ -13,8 +15,13 @@ let tallarinImage = new Image();
 tallarinImage.src = '../images/tallarin.png'
 
 // Declaración de variables
-let score = 7;
+let score = 36;
 let pizzas = [];
+let reinicioDelJuego;
+
+//const plato = new Plato (440,540,120,60,plateImage, ctx);
+//const tallarin = new Tallarin (0,599,canvas.width,10,tallarinImage, ctx);
+
 
 // Creación del objeto pizza usando Math.random
 // Metiendo cada pizza dentro del array de pizzas
@@ -43,32 +50,32 @@ const caerPizza = () => {
           if(score >= 0 && score <= 8){
             // Nivel 1 de dificultad
             pizza.borrar();
-            pizza.y += 4;
+            pizza.y += 6;
             pizza.dibujar();
           } else if (score > 8 && score <= 14){
             // Nivel 2 de  dificultad
             pizza.borrar();
-            pizza.y += 6;
+            pizza.y += 8;
             pizza.dibujar();
           } else if (score > 14 && score <= 20 ){
             // Nivel 3 de dificultad
             pizza.borrar
-            pizza.y += 8;
+            pizza.y += 10;
             pizza.dibujar();
           } else if (score > 20  && score <=26){
             // Nivel 4 de dificultad
             pizza.borrar();
-            pizza.y += 10;
+            pizza.y += 12;
             pizza.dibujar();
           } else if (score > 26 && score <= 32){ 
             // Nivel  5 de dificultad
             pizza.borrar();
-            pizza.y += 12;
+            pizza.y += 14;
             pizza.dibujar();
            } else {
             // Nivel supremo de dificultad
             pizza.borrar();
-            pizza.y += 14;
+            pizza.y += 16;
             pizza.dibujar();
            }
            if (pizza.detectarColision(plato)){
@@ -78,11 +85,16 @@ const caerPizza = () => {
             
             }  
           if (pizza.detectarColision(tallarin)){
+            ctx.clearRect(0, 0, 880, 620);
             pizza.borrar();
             pizzas.splice(i,1);
-            alert("GAME OVER");
-            document.location.reload();
-           
+            
+            //pizzas = [null];
+            
+            //alert("GAME OVER");
+            gameOver();
+            //reinicio();
+            //setTimeout(document.location.reaload, 1000);
           
           }
   }
@@ -113,6 +125,24 @@ const caerPizza = () => {
     ctx.fillText(`${score}`, 840, 590);
 }*/
 
+function gameOver (){
+  //lienzo.clearRect(0, 0, d.width, d.height);
+  /*ctx.font = "80px Cambria";
+  ctx.fillStyle = "#2C5B02";
+  ctx.fillText("GAME OVER", 100, 300);
+  ctx.fillText("Your final score is " + score, 100,  400);*/
+  ending.classList.remove("hidden")
+  canvasDiv.classList.add("hidden")
+}
+
+/*function reinicio (){
+  reinicioDelJuego = setTimeout(recargarJuego,4000);
+}
+
+function recargarJuego (){
+  document.location.reload
+}*/
+
 // Creación de la función que nos carga el juego
 // Creamos una nueva pizza cada 4s y chequeamos la situación del juego cada 0,5s
 // Dibujamos el plato, el tallarin y  la puntuación  en  el canvas
@@ -126,6 +156,13 @@ const caerPizza = () => {
 
   }
 
+  
+  function limpiarCanvas() {
+      ctx.clearRect(0, 0, 880, 620);
+ 
+   }
+  
+
 // Añadimos los listeners de mouse y teclado
 // Cuando hacemos click en el botón de start, añadimos el
 // menú del canvas y ocultamos el menú inicial
@@ -134,4 +171,7 @@ startButton.addEventListener("click", function(){
   divMenu.classList.add("hidden")
   canvasDiv.classList.remove("hidden")
 })
+restartButton.addEventListener("click", function(){
+  window.location.reload()
+});
 window.addEventListener("keydown", moverPlato);
